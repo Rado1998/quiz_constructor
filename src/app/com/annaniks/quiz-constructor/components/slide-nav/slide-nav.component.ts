0e6cbc09b58d31f90e700b88bfc286aff3b497df
Janna Mryan <janna.mryan@annaniks.com>
2019-10-16 17:25:07 +0400

    created QC_10

--- src/app/com/annaniks/quiz-constructor/components/slide-nav/slide-nav.component.ts
+++ src/app/com/annaniks/quiz-constructor/components/slide-nav/slide-nav.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { MenuService } from '../../services/menu.service';
 import { MenuItem } from '../../models/models';
+import { CookieService } from 'ngx-cookie';
+import { Router } from '@angular/router';
 
 @Component({
     selector: "app-slidenav",
@@ -11,12 +13,18 @@ import { MenuItem } from '../../models/models';
 export class SlideNavComponent implements OnInit {
 
     public menuItem: MenuItem[] =
-    [
-        { icon: "question_answer", title: "Questions", routerLink: "/questions" },
-        { icon: "exit_to_app", title: "Log Out", routerLink: "/auth/login" },
-    ] 
+        [
+            { icon: "question_answer", title: "Questions", routerLink: "/questions" },
+        ]
 
-    constructor(public menuService:MenuService) { }
+    constructor(public menuService: MenuService, private _cookieService: CookieService, private _router: Router) { }
 
     ngOnInit() { }
+
+    public logOut(): void {
+        this._cookieService.remove('accessToken');
+        this._router.navigate(['/auth/login']);
+
+    }
+
 }
