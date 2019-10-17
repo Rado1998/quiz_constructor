a1c5f25734a968dde1070adc0192bd07a0e0cb2c
Janna Mryan <janna.mryan@annaniks.com>
2019-10-17 12:27:22 +0400

    created question-details page

--- src/app/com/annaniks/quiz-constructor/views/main/question/question.component.ts
+++ src/app/com/annaniks/quiz-constructor/views/main/question/question.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-
 import { QuestionService } from './question.service';
+import { Router } from '@angular/router';
+
 
 
 @Component({
@@ -12,14 +13,19 @@ import { QuestionService } from './question.service';
 export class QuestionComponent implements OnInit {
 
   response: Observable<any> | any;
-  constructor(private _uestionService: QuestionService) { }
+  constructor(private _uestionService: QuestionService,private _router:Router) { }
 
   ngOnInit() {
   }
-  getQuestions() {
 
+  getQuestions() {
     this.response = this._uestionService.gets('answers/').pipe(  )
       .subscribe(data => console.log(data))
   }
+
+ public openQuestionDetailPage(){
+   this._router.navigate(['/questions/1'])
+   
+ }
 
 }
